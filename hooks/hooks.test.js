46255aefa3f518503284be1b9b20d05ab3b18a22
mrsherpacode <sherpacoding@gmail.com>
2026-05-11 14:48:41 -0500

    testing hook is used for testing

---
+++ hooks/hooks.test.js
@@ -0,0 +1,40 @@
+import { it, expect, beforeAll, beforeEach, afterEach, afterAll } from "vitest";
+
+import { User } from "./hooks";
+const testEmail = "test@test.com";
+let user = new User(testEmail);
+
+//Here, i'm using afterEach() testing hook
+// afterEach runs after every individual test in the current scope and Clean up after each test (so tests don't affect each other).
+
+afterEach(() => {
+  user = new User(testEmail);
+});
+
+it("should update the email", () => {
+  const newTestEmail = "test2@test.com";
+
+  user.updateEmail(newTestEmail);
+
+  expect(user.email).toBe(newTestEmail);
+});
+
+it("should have an email property", () => {
+  expect(user).toHaveProperty("email");
+});
+
+it("should store the provided email value", () => {
+  expect(user.email).toBe(testEmail);
+});
+
+it("should clear the email", () => {
+  user.clearEmail();
+
+  expect(user.email).toBe("");
+});
+
+it("should still have an email property after clearing the email", () => {
+  user.clearEmail();
+
+  expect(user).toHaveProperty("email");
+});
